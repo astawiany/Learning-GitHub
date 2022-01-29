a2ac154b5d8b158a79690e1e1a4098c8c3c82db5
Adrian Stawiany <astawiany@gmail.com>
2022-01-29 16:36:08 +0100

    Initial Commit

---
+++ LearningGitHub/LearningGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGitHubApp.swift
+//  LearningGitHub
+//
+//  Created by Adrian Stawiany on 29/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
